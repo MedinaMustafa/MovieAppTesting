49304ed8535a1720964f6435c555232bed28852b
Medina Mustafa <medinaamustafa@gmail.com>
2024-04-14 01:16:57 +0200

    komiitii

--- MovieApp/obj/Release/net8.0/MovieApp.AssemblyInfo.cs
+++ MovieApp/obj/Release/net8.0/MovieApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+674ef206d21a974dffb6f743d75125019e127a45")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
