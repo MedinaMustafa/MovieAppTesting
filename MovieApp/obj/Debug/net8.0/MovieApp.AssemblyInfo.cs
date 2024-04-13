7955d58756b11950303ebb55a191996f26892b95
Rron <rronihhh@gmail.com>
2024-04-14 01:55:21 +0200

    223

--- MovieApp/obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
+++ MovieApp/obj/Debug/net8.0/MovieApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49304ed8535a1720964f6435c555232bed28852b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e35eaf65d89e14123a07f5276fbab653e6526d2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
